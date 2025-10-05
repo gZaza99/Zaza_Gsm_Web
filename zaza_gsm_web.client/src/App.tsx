ced372038146b92a459bf24feefd586ca29d7954
ZAZA-LAPTOP\zalan <zalan.gal.99@gmail.com>
2025-10-05 11:02:20 +0200

    Try to fix CI/CD

--- zaza_gsm_web.client/src/App.tsx
+++ zaza_gsm_web.client/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
 import NavBar from './components/NavBar'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <NavBar/>
