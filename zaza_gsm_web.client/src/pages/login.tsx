0c3cae1db302540df2059acb37c5e15edb61ce20
ZAZA-LAPTOP\zalan <zalan.gal.99@gmail.com>
2025-10-20 17:59:17 +0200

    Use runChecksAndShowErrors() method on Login page too.

--- zaza_gsm_web.client/src/pages/login.tsx
+++ zaza_gsm_web.client/src/pages/login.tsx
@@ -1,9 +1,16 @@
-import type { IClient } from "../interfaces/iclient";
+import { runChecksAndShowErrors } from "../components/common";
+import type { IUser } from "../interfaces/iuser";
 import { useState } from "react";
 
 export default function Login() {
     const [usrname, setUsrName] = useState<string>("");
     const [passwd, setPasswd] = useState<string>("");
+    const user: IUser = {
+        id: null,
+        username: null,
+        email: "",
+        password: ""
+    };
     return (
         <div className="p-2 pt-0 flex justify-center">
         <div className="bg-[#222] text-white rounded-2xl shadow-lg p-4 w-full border border-gray-700">
@@ -13,21 +20,27 @@ export default function Login() {
 
             <form className="space-y-6">
                 {/* Username */}
-                <div>
+                <div id="usernameSection">
                     <label className="block text-sm text-gray-400 mb-1">Felhasználónév / email cím *</label>
                     <input
                     type="text"
-                    onChange={e => setUsrName(e.target.value)}
+                    onChange={e => {
+                        setUsrName(e.target.value);
+                        user.username = e.target.value;
+                    }}
                     className="w-full bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white focus:ring-2 focus:ring-blue-600 outline-none"
                     />
                 </div>
 
                 {/* Jelszó */}
-                <div>
+                <div id="passwordSection">
                     <label className="block text-sm text-gray-400 mb-1">Jelszó *</label>
                     <input
                     type="password"
-                    onChange={e => setPasswd(e.target.value)}
+                    onChange={e => {
+                        setPasswd(e.target.value);
+                        user.password = e.target.value;
+                    }}
                     className="w-full bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white focus:ring-2 focus:ring-blue-600 outline-none"
                     />
                 </div>
@@ -38,7 +51,10 @@ export default function Login() {
                     type="submit"
                     onClick={e => {
                         e.preventDefault();
-                        onLogInPressed(usrname, passwd);
+                        runChecksAndShowErrors(
+                            () => checkUsername(usrname),
+                            () => checkPassword(passwd)
+                        );
                     }}
                     className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded-lg grow"
                     >
@@ -56,6 +72,18 @@ export default function Login() {
     );
 }
 
-function onLogInPressed(usrName: string, passwd: string) {
-    
+function checkUsername(usrName: string): [HTMLDivElement, string] | [HTMLDivElement, string[]] | HTMLDivElement {
+    let section = document.getElementById("usernameSection") as HTMLDivElement;
+    if (usrName == "")
+        return [section, "Felhasználónév kitöltése kötelező"];
+    else
+        return section;
+}
+
+function checkPassword(password: string): [HTMLDivElement, string] | [HTMLDivElement, string[]] | HTMLDivElement {
+    let section = document.getElementById("passwordSection") as HTMLDivElement;
+    if (password == "")
+        return [section, "Jelszó kitöltése kötelező"];
+    else
+        return section;
 }
